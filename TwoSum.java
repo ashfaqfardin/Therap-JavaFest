ba73a28ea7d612f8e29cf5fd7137cb51ce867475
ashfaqfardin <imashfaqfardin@gmail.com>
2023-06-24 19:08:59 +0600

    --update

--- TwoSum.java
+++ TwoSum.java
@@ -1,6 +1,6 @@
 import java.util.HashMap;
 
-class Solution{
+class TwoSum{
     public int[] twoSum(int[] nums, int target){
         int[] ans = new int[2];
         HashMap<Integer, Integer> map = new HashMap<>();
