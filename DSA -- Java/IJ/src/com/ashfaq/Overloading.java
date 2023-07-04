8216f5e47ac494d8f48d870bdc7dc8e9287da67b
ashfaqfardin <imashfaqfardin@gmail.com>
2023-07-04 06:56:26 +0600

    -- function/method overloading

---
+++ DSA -- Java/IJ/src/com/ashfaq/Overloading.java
@@ -0,0 +1,33 @@
+package com.ashfaq;
+
+import java.util.Arrays;
+
+public class Overloading {
+    public static void main(String[] args) {
+        fun(67);
+        fun("Ashfaq");
+        fun("Ashfaq", "Fardin");
+        fun(1, 2, 3, 5);
+        fun("Ashfaq", "Rahman", "Gmail");
+    }
+
+    static void fun(int ...v){
+        System.out.println(Arrays.toString(v));
+    }
+    static void fun(String ...v){
+        System.out.println(Arrays.toString(v));
+    }
+
+    static void fun(int a){
+        System.out.println(a);
+    }
+
+    static void fun(String name){
+        System.out.println(name);
+    }
+
+    static void fun(String name, String id){
+        System.out.println(name);
+        System.out.println(id);
+    }
+}
