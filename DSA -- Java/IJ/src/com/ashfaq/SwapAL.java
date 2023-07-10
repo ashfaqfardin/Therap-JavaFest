be0e25a630d537e74a2b3bfe7e349a7c34e4c217
ashfaqfardin <imashfaqfardin@gmail.com>
2023-07-10 08:29:02 +0600

    -- Array Element Swap

---
+++ DSA -- Java/IJ/src/com/ashfaq/SwapAL.java
@@ -0,0 +1,18 @@
+package com.ashfaq;
+
+import java.util.Arrays;
+
+public class SwapAL {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 43, 53, 64};
+        swap(arr, 1, 3);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void swap(int[] arr, int index1, int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+}
