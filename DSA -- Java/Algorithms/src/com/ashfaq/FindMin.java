7cec6543993ca930e46bf50f3b0ce8821e019bd5
ashfaqfardin <imashfaqfardin@gmail.com>
2023-08-01 03:29:17 +0600

    -- Algorithm -- FindMin

---
+++ DSA -- Java/Algorithms/src/com/ashfaq/FindMin.java
@@ -0,0 +1,20 @@
+package com.ashfaq;
+
+public class FindMin {
+    public static void main(String[] args) {
+        int[] arr = {18, 12, -7, 3, 14, 28, 11};
+        System.out.println(min(arr));
+    }
+
+    // assume arr.length != 0
+    // return the min val in the array
+    static int min(int[] arr){
+        int ans = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < ans){
+                ans = arr[i];
+            }
+        }
+        return ans;
+    }
+}
